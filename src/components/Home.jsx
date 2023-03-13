cc56dae5c49d2fbbe3ba3876392220c45264977e
kswitch <kswitch2007@gmail.com>
2023-03-13 22:16:18 +0100

    Added hover to reveal fav and cart icons in App

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,36 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { ContextConsumer } from './contexts/Context'
 
 import Loading from './loading/Loading'
 
 function Home() {
     const {images, handleFavorites, handleCartItems} = useContext(ContextConsumer)
-
-    const renderImages = images.map(image => (
-        <div className='home-images' key={image.id} id={image.id}>
-            <img 
-                className="image" 
-                id={image.id} 
-                src={image.url}
-            />
-            <div className='home-images-icons'>
-                <i 
-                    className={`fav-icon ${image.isFavorite ? 'ri-heart-fill' : 'ri-heart-line'}`}
-                    onClick={(e) => handleFavorites(e.target.parentElement.parentElement.id)}
-                ></i>
-                <i 
-                    className={`cart-icon ${image.isInCart ? 'ri-shopping-cart-fill' : 'ri-add-circle-line'}`}
-                    onClick={(e) => handleCartItems(e.target.parentElement.parentElement.id)}
-                ></i>
+    
+    const renderImages = images.map(image => {
+        const [hovered, setHovered] = useState(false)
+        return (
+            <div className='home-images' key={image.id} id={image.id} onMouseEnter={()=>setHovered(true)} onMouseLeave={()=>setHovered(false)}>
+                <img 
+                    className="image" 
+                    id={image.id} 
+                    src={image.url}
+                />
+                {hovered &&
+                    <div className='home-images-icons'>
+                        <i 
+                            className={`fav-icon ${image.isFavorite ? 'ri-heart-fill' : 'ri-heart-line'}`}
+                            onClick={(e) => handleFavorites(e.target.parentElement.parentElement.id)}
+                        ></i>
+                        <i 
+                            className={`cart-icon ${image.isInCart ? 'ri-shopping-cart-fill' : 'ri-add-circle-line'}`}
+                            onClick={(e) => handleCartItems(e.target.parentElement.parentElement.id)}
+                        ></i>
+                    </div>
+                }
             </div>
-        </div>
-    ))
+        )
+    })
+        
 
     return (
         <div className="home">
--- src/components/favorites/FavoriteList.jsx
+++ src/components/favorites/FavoriteList.jsx
@@ -1,28 +1,34 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {ContextConsumer} from './../contexts/Context'
 
 function FavoriteList() {
     const {favItems, handleFavorites, handleCartItems} = useContext(ContextConsumer)
 
-    const favList = favItems.map(item => (
-        <div className='home-images fav-images' key={item.id} id={item.id}>
-            <img 
-                className="image" 
-                id={item.id} 
-                src={item.url}
-            />
-            <div className='home-images-icons'>
-                <i 
-                    className={`cart-item-delete-btn ${item.isFavorite ? 'ri-dislike-fill' : 'ri-heart-line'}`}
-                    onClick={(e) => handleFavorites(e.target.parentElement.parentElement.id)}
-                ></i>
-                <i 
-                    className={`cart-icon ${item.isInCart ? 'ri-shopping-cart-fill' : 'ri-add-circle-line'}`}
-                    onClick={(e) => handleCartItems(e.target.parentElement.parentElement.id)}
-                ></i>
+    const favList = favItems.map(item => {
+        // const [favHovered, setFavHovered] = useState(false)
+        // onMouseEnter={()=>setFavHovered(true)} onMouseLeave={()=>setFavHovered(false)}
+        return (
+            <div className='home-images fav-images' key={item.id} id={item.id} >
+                <img 
+                    className="image" 
+                    id={item.id} 
+                    src={item.url}
+                />
+                {/* {favHovered && */}
+                    <div className='home-images-icons'>
+                        <i 
+                            className={`cart-item-delete-btn ${item.isFavorite ? 'ri-dislike-fill' : 'ri-heart-line'}`}
+                            onClick={(e) => handleFavorites(e.target.parentElement.parentElement.id)}
+                        ></i>
+                        <i 
+                            className={`cart-icon ${item.isInCart ? 'ri-shopping-cart-fill' : 'ri-add-circle-line'}`}
+                            onClick={(e) => handleCartItems(e.target.parentElement.parentElement.id)}
+                        ></i>
+                    </div>
+                {/* } */}
             </div>
-        </div>
-    ))
+        )
+    })
 
     return (
         <div className="favorite-list">
